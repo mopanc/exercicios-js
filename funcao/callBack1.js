db1e24a56b78b8ff3a10e48735811640dc82bfcc
Jorge Morais <jorgemorais@iMac-de-Jorge.local>
2020-04-08 21:49:54 -0300

    arrow calback

---
+++ funcao/callBack1.js
@@ -0,0 +1,8 @@
+const fabricantes = ["Mecedes", "Audi", "BMW", "Jeep"]
+
+function imprimir(nome, indice) {
+    console.log(`${indice + 1}. ${nome}`)
+}
+
+fabricantes.forEach(imprimir)
+fabricantes.forEach(fabricante => console.log(fabricante))
--- funcao/paramPadrao.js
+++ funcao/paramPadrao.js
@@ -17,3 +17,10 @@ function soma2(a, b, c) {
 }
 
 console.log(soma2(), soma2(3), soma2(1, 2, 3), soma2(0, 0, 0))
+
+// Valor Padrão do es2015
+function soma3(a = 1, b = 1, c = 1) {
+    return a + b + c
+}
+
+console.log(soma3(), soma3(3), soma3(1, 2, 3), soma3(0, 0, 0))
